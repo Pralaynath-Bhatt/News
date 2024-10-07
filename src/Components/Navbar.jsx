01e5a2c3487654baf73ba2bb7f91f1cc82bc388f
pralaynath <pranjalbhattt04@gmail.com>
2024-10-08 04:30:18 +0530

    website styled

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -5,46 +5,55 @@ import img from "./logo.png"
   let [query,setQuery]= useState("");
   return (
     
-    <nav className="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark" style={{position:"fixed",zIndex:"3", width:"100%"}}>
-  <div className="container-fluid">
-    <img className="logo" src={img} alt='logo' style={{height:"50px",width:"100px"}}></img>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarNav">
-      <ul className="navbar-nav">
-      <li className="nav-item">
-          <a className="nav-link" onClick={()=>setCategory("general")} >General</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" onClick={()=>setCategory("technology")}>Technology</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" onClick={()=>setCategory("business")}>Business</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" onClick={()=>setCategory("health")}>Health</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" onClick={()=>setCategory("science")}>Science</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" onClick={()=>setCategory("sports")}>Sports</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" onClick={()=>setCategory("entertainment")}>Entertainment</a>
-        </li>    
-      </ul>
+    <nav className="navbar navbar-expand-lg bg-dark" style={{ position: "fixed", zIndex: "3", width: "100%", padding: "10px 20px" }}>
+    <div className="container-fluid">
+      <img className="logo" src={img} alt='logo' style={{ height: "50px", width: "100px", marginRight: "20px" }} />
+      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarNav">
+        <ul className="navbar-nav">
+          {["general", "technology", "business", "health", "science", "sports", "entertainment"].map((category) => (
+            <li className="nav-item" key={category}>
+              <a className="nav-link" onClick={() => setCategory(category)}>{category.charAt(0).toUpperCase() + category.slice(1)}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
+  
+      <div className="search-container" style={{ borderRadius: "20px", display: "flex", alignItems: "center", backgroundColor: '#ffffff', padding: "5px 10px" }}>
+        <input
+          type="search"
+          placeholder='Search'
+          onChange={(e) => setQuery(e.target.value)}
+          style={{
+            color: "black",
+            borderRadius: "10px",
+            border: "1px solid #ccc",
+            backgroundColor: "white",
+            height: "40px",
+            padding: "0 10px",
+            outline: "none"
+          }}
+        />
+        <button
+          onClick={() => setSearch(query)}
+          style={{
+            borderRadius: "10px",
+            backgroundColor: "#e6e6e6",
+            border: "none",
+            height: "40px",
+            padding: "0 15px",
+            cursor: "pointer",
+            marginLeft: "5px",
+          }}
+        >
+          🔍
+        </button>
+      </div>
     </div>
-    
-                <div style={{borderRadius:"20px",height:"40px",display:"flex",backgroundColor:'white'}}>
-                <input type="search" placeholder='Search'
-                onChange={(e)=>{setQuery(e.target.value)}}
-                style={{color:"black",textSizeAdjust:"auto",borderRadius:"10px",borderStyle:"hidden",backgroundColor:"white",height:"40px"}}/>
-                <button onClick={()=>{setSearch(query)}} style={{borderRadius:"50%",backgroundColor:"#e6e6e6"}}>🔍</button>
-                </div>
-  </div>
   </nav>
+  
   )
 }
 export default Navbar;
--- src/Components/news.jsx
+++ src/Components/news.jsx
@@ -5,20 +5,29 @@ import "./newsCard.css"
     src = `https://cdn.pixabay.com/photo/2015/02/15/09/33/news-636978_1280.jpg`
   }
   return (
-    <div id="Card" className="card bg-dark text-light mb-3 d-inline-block mx-3 px-2 my-3 py-2" style={{maxWidth:"330px",minHeight:"390px",borderRadius:"10px"}}>
-    <img src={src} style={{height:"200px",width:"314px"}} className="card-img-top" alt="image"/>
-    <div className="card-body">
-      <div className='info' style={{display:"flex",justifyContent:"space-between",alignItems:"center",padding:"10px 0px 0px 0px"}}>
-        <p>{site.slice(0,20)}</p>
-        <p>⌚{publishedAt.slice(0,10)}</p>
+    <div id="Card" className="card mb-3 d-inline-block mx-3 my-3" style={{ maxWidth: "330px", borderRadius: "10px", overflow: "hidden", height: "auto" }}>
+  <img src={src} className="card-img-top" alt="image" style={{ height: "200px", objectFit: "cover" }} />
+  <div className="card-body bg-dark text-light" style={{ padding: "20px", display: "flex", flexDirection: "column", justifyContent: "space-between", height: "300px" }}>
+    <div>
+      <div className="info" style={{ display: "flex", justifyContent: "space-between", marginBottom: "10px" }}>
+        <p className="m-0">{site.slice(0, 20)}</p>
+        <p className="m-0">⌚ {publishedAt.slice(0, 10)}</p>
       </div>
-      <h5 className="card-title">{title.length>70?title.slice(0,70)+"...":title}</h5>
-      <p className="card-text">{content?content.slice(0,90)+"...":"Sorry no content please click the read more button below to know more about the article"}</p>
-      <p>by {author?author.slice(0,20):"Anoynamous"}</p>
-      <a href={url} className="btn btn-primary">Read More</a>
-      
+      <h5 className="card-title" style={{ fontSize: "1.2rem", marginBottom: "10px" }}>
+        {title.length > 70 ? title.slice(0, 70) + "..." : title}
+      </h5>
+      <p className="card-text" style={{ marginBottom: "15px" }}>
+        {content ? content.slice(0, 90) + "..." : "Sorry no content. Please click the read more button below to know more about the article."}
+      </p>
+      <p className="text" style={{ fontSize: "0.9rem", marginBottom: "15px" }}>
+        by {author ? author.slice(0, 20) : "Anonymous"}
+      </p>
     </div>
+    <a href={url} className="btn btn-primary" style={{ width: "100%" }}>Read More</a>
   </div>
+</div>
+
+
   )
 }
 export default News;
